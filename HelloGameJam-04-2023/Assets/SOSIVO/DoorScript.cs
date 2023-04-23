1642368b40becfa949fbdd6ff03fac63c07a1980
Axyu01 <adriansosniak01@gmail.com>
2023-04-24 00:02:07 +0200

    rotator changed

--- HelloGameJam-04-2023/Assets/SOSIVO/DoorScript.cs
+++ HelloGameJam-04-2023/Assets/SOSIVO/DoorScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DoorScript : InteractableObject
 {
+    [SerializeField]
+    GameObject rotator;
+    [SerializeField]
+    Quaternion rotation1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class DoorScript : InteractableObject
     // Update is called once per frame
     void Update()
     {
-        
+        rotator.transform.rotation *= Quaternion.Euler(new Vector3(0f, 90f, 0f) * Time.deltaTime);
     }
 }
