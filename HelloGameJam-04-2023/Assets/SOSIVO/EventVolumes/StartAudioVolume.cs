5ec6cb4c4e9f842946fba9209f982ae3bec36f6f
Axyu01 <adriansosniak01@gmail.com>
2023-04-25 21:29:02 +0200

    Big Changes

---
+++ HelloGameJam-04-2023/Assets/SOSIVO/EventVolumes/StartAudioVolume.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StartAudioVolume : MonoBehaviour
+{
+    [SerializeField]
+    VolumeType volumeType;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    bool playFlag = true;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (playFlag == false)
+            return;
+        if (volumeType == VolumeType.Narration)
+        { 
+            var temp = other.GetComponent<AudioSource>();
+            temp.clip=audioSource.clip;
+            temp.loop = false;
+            temp.Play();
+            Destroy(gameObject);
+        }
+        if(volumeType==VolumeType.OneTime)
+        {
+            audioSource.loop = false;
+            audioSource.Play();
+            playFlag= false;
+        }
+        if(volumeType == VolumeType.EnterRepeatable)
+        {
+            audioSource.loop = false;
+            audioSource.Play();
+        }
+    }
+    [SerializeField]
+    public enum VolumeType
+    {
+        OneTime,EnterRepeatable,Narration
+    }
+}
